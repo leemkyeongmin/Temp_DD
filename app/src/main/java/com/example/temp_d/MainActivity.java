19290eab300fa893f52635660a31737a12c5b63d
leemkyeongmin <pproac2@gmail.com>
2022-03-13 01:46:51 +0900

    success 123

--- app/src/main/java/com/example/temp_d/MainActivity.java
+++ app/src/main/java/com/example/temp_d/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     
     // 네번쨰 주석
      /// 다섯번째
+    
+    // 여섯번째 성공기념
     /* EDT에 화씨를 입력 -> '변환'버튼 클릭 -> 섭씨로 변환
      리니어 레이아웃 3개, 컴포넌트 3개 (EDT, TXTV, BTN)
      리니어 레이아웃 큰 틀 1개(vertical), 그 안에 2개는 horizontal로 정렬 */
